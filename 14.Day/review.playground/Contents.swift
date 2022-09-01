82d462bb797557c17048b9b0ac5bc367665703ef
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-09-01 16:36:21 +0300

    Day 14

---
+++ 14.Day/review.playground/Contents.swift
@@ -0,0 +1,161 @@
+import Cocoa
+
+func favoriteAlbum() {
+    print("My favorite is Fearless")
+}
+
+favoriteAlbum()
+
+func favoriteAlbum(name: String) {
+    print("My favorite is \(name)")
+}
+
+favoriteAlbum(name: "Fearless")
+
+func printAlbumRelease(name: String, year: Int) {
+    print("\(name) was released in \(year)")
+}
+
+printAlbumRelease(name: "Fearless", year: 2008)
+printAlbumRelease(name: "Speak Now", year: 2010)
+printAlbumRelease(name: "Red", year: 2012)
+
+func countLettersInString(string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+
+countLettersInString(string: "Hello")
+
+func countLettersInString(_ str: String) {
+    print("The string \(str) has \(str.count) letters.")
+}
+
+countLettersInString("Hello")
+
+func countLetters(in string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+
+countLetters(in: "Hello")
+
+func albumIsTaylor(name: String) -> Bool {
+    if name == "Taylor Swift" { return true }
+    if name == "Fearless" { return true }
+    if name == "Speak Now" { return true }
+    if name == "Red" { return true }
+    if name == "1989" { return true }
+
+    return false
+}
+
+func getMeaningOfLife() -> Int {
+    return 42
+}
+
+func getHaterStatus() -> String {
+    return "Hate"
+}
+
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny" {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+func takeHaterAction(status: String) {
+    if status == "Hate" {
+        print("Hating")
+    }
+}
+
+if let haterStatus = getHaterStatus(weather: "rainy") {
+    takeHaterAction(status: haterStatus)
+}
+
+func yearAlbumReleased(name: String) -> Int? {
+    if name == "Taylor Swift" { return 2006 }
+    if name == "Fearless" { return 2008 }
+    if name == "Speak Now" { return 2010 }
+    if name == "Red" { return 2012 }
+    if name == "1989" { return 2014 }
+
+    return nil
+}
+
+
+enum WeatherType {
+    case sun
+    case cloud
+    case rain
+    case wind
+    case snow
+}
+
+func getHaterStatusXC(weather: WeatherType) -> String? {
+    switch weather {
+        case .sun:
+            return nil
+        case .cloud, .wind:
+            return "dislike"
+        case .rain:
+            return "hate"
+    case .snow:
+        return "snow"
+    }
+}
+
+getHaterStatusXC(weather: .cloud)
+
+struct Person {
+    var clothes: String
+    var shoes: String
+    
+    func describe() {
+            print("I like wearing \(clothes) with \(shoes)")
+        }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+var taylorCopy = taylor
+taylorCopy.shoes = "flip flops"
+
+print(taylor)
+print(taylorCopy)
+
+class Singer {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func sing() {
+        print("La la la la")
+    }
+}
+
+class CountrySinger: Singer {
+    override func sing() {
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+class HeavyMetalSinger: Singer {
+    var noiseLevel: Int
+
+    init(name: String, age: Int, noiseLevel: Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
+
