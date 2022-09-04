a033015b7a9dbfc2f61c82afceea655d0149c9b1
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-09-04 07:54:01 +0300

    project3

---
+++ 22.Day/challenge 3/project2/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Ömer Faruk Öztürk on 2.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var scoreText: UILabel!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var questionCount = 0
+    var negative = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.tintColor = UIColor.red
+        // Do any additional setup after loading the view.
+        
+        btn1.layer.borderWidth = 3
+        btn2.layer.borderWidth = 5
+        btn3.layer.borderWidth = 3
+        
+        btn1.layer.borderColor = UIColor.lightGray.cgColor
+        btn2.layer.borderColor = UIColor(red: 1.0, green: 0.7, blue: 0.5, alpha: 1).cgColor
+        btn3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+        
+        scoreText.textColor = UIColor.red
+        scoreText.font.withSize(24)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score", style: .plain, target: self, action: #selector(scoreTapped))
+    }
+
+    
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        print(correctAnswer)
+        title = countries[correctAnswer].uppercased()
+        
+        btn1.setImage(UIImage(named: countries[0]), for: .normal)
+        btn2.setImage(UIImage(named: countries[1]), for: .normal)
+        btn3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title = ""
+        print(sender.tag)
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+            scoreText.text = String(score)
+            questionCount += 1
+        } else {
+            title = "Wrong!"
+            score -= 1
+            scoreText.text = String(score)
+            questionCount += 1
+            negative += 1
+            
+            let warningAlert = UIAlertController(title: "Wrong!", message: "This is \(countries[sender.tag].uppercased()) flag.", preferredStyle: .alert)
+            warningAlert.addAction(UIAlertAction(title: "OKAY", style: .default, handler: askQuestion))
+            present(warningAlert, animated: true)
+        }
+        
+        if questionCount == 10 {
+            let questionWarning = UIAlertController(title: "10 Question", message: "Total 10 Question => \(questionCount-negative) / \(questionCount) and Score: \(score)", preferredStyle: .alert)
+            questionWarning.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(questionWarning, animated: true)
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
+    }
+    @objc func scoreTapped(){
+        let ac = UIAlertController(title: "Score", message: "Your score is \(score) \n Your positive number is \(questionCount - negative) \n Your negative number is \(negative) \n \n Total question number is \(questionCount)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+}
+
