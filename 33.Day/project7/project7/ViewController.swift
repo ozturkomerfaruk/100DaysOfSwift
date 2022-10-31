9776fc96bb7ad77236e1c7ca16b40f5fc73badd4
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-10-31 19:58:53 +0300

    33. Gün

---
+++ 33.Day/project7/project7/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Ömer Faruk Öztürk on 31.10.2022.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+           let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+
+           if let url = URL(string: urlString) {
+               if let data = try? Data(contentsOf: url) {
+                   parse(json: data)
+               }
+           }
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    
+}
+
