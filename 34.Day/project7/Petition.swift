38e1c3e9f2b143d3aa6370c8e797d2c7eb9cd6bb
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-11-01 11:21:48 +0300

    34. Gün

---
+++ 34.Day/project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Ömer Faruk Öztürk on 31.10.2022.
+//
+
+import Foundation
+
+struct Petition : Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ 34.Day/project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Ömer Faruk Öztürk on 31.10.2022.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
